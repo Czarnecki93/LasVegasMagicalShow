9d1c17c61f2e3bb5f2be671e1a1141377a698e37
Patryk Czarnecki <cph-pc97@cphbusiness.dk>
2020-10-04 03:11:31 +0200

    Added Magician class.

---
+++ Magician.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LasVegasMagicalShow
+{
+    public class Magician : Person
+    {
+        protected ArrayList favorite_tricks;
+
+        // Constructor
+        public Magician(int id, string name, string password) : base(id, name, password)
+        {
+            favorite_tricks = new ArrayList();
+        }
+
+        // Add favorite trick(s)
+        public void AddFavoriteTrick(string trick)
+        {
+            favorite_tricks.Add(trick);
+        }
+    }
+}
